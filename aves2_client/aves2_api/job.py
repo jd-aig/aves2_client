6a21dffdd2e95a270424f2d3d4edfea308067aa8
Yang Haibo <yanghaibo1@jd.com>
2020-03-23 00:25:50 +0800

    check client version before real command

--- aves2_client/aves2_api/job.py
+++ aves2_client/aves2_api/job.py
@@ -5,6 +5,8 @@ import logging
 import requests
 
 from aves2_client.settings import aves2_cfg
+from aves2_client.version import aves2_version
+from aves2_client.err import *
 
 
 AVES2_HOST = aves2_cfg['Aves2Server']['host']
@@ -17,6 +19,7 @@ AVES2_APIS = {
     'cancel_job': '/api/aves_job/{jobid}/cancel_job/',
     'log_job': '/api/aves_job/{jobid}/logs/',
     'delete_job': '/api/aves_job/{jobid}/',
+    'client_check': '/api/client_check',
 }
 
 HEADERS = {
@@ -27,37 +30,50 @@ HEADERS = {
 def url_join(host, uri):
     return os.path.join(host, uri.lstrip('/'))
 
+def client_check():
+    url = url_join(AVES2_HOST, AVES2_APIS['client_check'])
+    rst = requests.get(url, params=dict(version=aves2_version()))
+    if rst.ok and not rst.json().get('success'):
+        raise Aves2ClientError('Please upgrade aves2 client')
+
 def list_job():
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['create_job'])
     rst = requests.get(url, headers=HEADERS)
     return rst
 
 def create_job(job_data):
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['create_job'])
     rst = requests.post(url, json=job_data, headers=HEADERS)
     return rst
 
 def get_job(jobid):
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['get_job']).format(**locals())
     rst = requests.get(url, headers=HEADERS)
     return rst
 
 def start_job(jobid):
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['start_job']).format(**locals())
     rst = requests.get(url, headers=HEADERS)
     return rst
 
 def cancel_job(jobid):
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['cancel_job']).format(**locals())
     rst = requests.get(url, headers=HEADERS)
     return rst
 
 def delete_job(jobid):
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['delete_job']).format(**locals())
     ret = requests.delete(url, headers=HEADERS)
     return ret
 
 def log_job(jobid):
+    client_check()
     url = url_join(AVES2_HOST, AVES2_APIS['log_job']).format(**locals())
     ret = requests.get(url, headers=HEADERS)
     return ret
